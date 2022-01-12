c7907f916d522ba1499119243d85fbdf4f78c54f
19120705 <tranviettuan2k1@gmail.com>
2022-01-12 20:40:23 +0700

    6 commit

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Nguyen Quoc Tuan-19120702 && Tran Viet Tuan-19120705 Project CI/CD\n");
+  response.end("19120702-19120705 test Project CI/CD\n");
 
 });
 
