f307772a604cf454723c7e68a4e49bc13113a527
19120705 <tranviettuan2k1@gmail.com>
2022-01-12 20:30:52 +0700

    4 commit

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ var http = require('http');
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Nguyen Quoc Tuan-19120702 && Tran Viet Tuan-19120705 \n");
-  adasdădad
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Nguyen Quoc Tuan-19120702 && Tran Viet Tuan-19120705 \n");
 });
 
 // listen on localhost:8000
